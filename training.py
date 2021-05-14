54ee1f8acbe739a214b8884d031827cca923ef76
Charles <adeakinyemi129@gmail.com>
2021-05-14 13:22:25 +0300

    later commit

--- training.py
+++ training.py
@@ -1,8 +1,10 @@
 import random
 import json
 import pickle
+from torch.optim.optimizer import Optimizer
 
 import nltk
+nltk.download('wordnet')
 import numpy as np;
 
 from nltk.stem import WordNetLemmatizer
@@ -10,7 +12,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
 words = []
@@ -18,10 +20,54 @@ classes = []
 documents = []
 ignore_letters = ['?','!','.',',']
 
+
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word_list = nltk.tokenize(pattern)  #tokenize splits words into individual word
-        words.append(word_list)
-        documents.append((word_list), intent['tag'])
+        word_list = nltk.word_tokenize(pattern)  #tokenize splits words into individual word
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+word = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl','wb'))
+pickle.dump(words, open('classes.pkl','wb'))
+
+training = []
+output_empty =[0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        if word in words:
+            bag.append(1) if words in word_patterns else bag.append(0)
+
+        output_row = list(output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu')) #dense layer of 64 neurons
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
